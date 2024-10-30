e32c50ef6cdc2e407a65629c02bc22d9090753c8
vegar <vegar1201@live.no>
2024-10-30 14:33:58 +0000

    Add own function for creating transformationMatrix.
    The function creates a full zyx rotation using Euler angles.

--- assignments_ws/src/iterative_closest_point/include/iterative_closest_point/icp.h
+++ assignments_ws/src/iterative_closest_point/include/iterative_closest_point/icp.h
@@ -1,13 +1,13 @@
 #ifndef ICP_H
 #define ICP_H
 
-#include "pcl/point_cloud.h"
-#include "ros/node_handle.h"
-#include "sensor_msgs/PointCloud2.h"
+#include <pcl/point_cloud.h>
+#include <ros/node_handle.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <ros/ros.h>
 
 #include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
+// #include <pcl_conversions/pcl_conversions.h>
 
 typedef pcl::PointXYZ PCLPoint;
 typedef pcl::PointCloud<PCLPoint> PCLPointCloud;
@@ -16,7 +16,7 @@ class ICP {
 
     public:
     ICP(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
-    bool applyICM(PCLPointCloud::Ptr &pclCloudIn, PCLPointCloud::Ptr &pclCloudOut);
+    bool applyICM(PCLPointCloud::Ptr &pclCloudIn, PCLPointCloud::Ptr &pclCloudOut, Eigen::Matrix4d &transformationMatrix);
     void callback(const sensor_msgs::PointCloud2ConstPtr &cloud);
 
     private:
--- assignments_ws/src/iterative_closest_point/src/icp.cpp
+++ assignments_ws/src/iterative_closest_point/src/icp.cpp
@@ -1,17 +1,53 @@
 #include "iterative_closest_point/icp.h"
-#include "ros/console.h"
+#include "Eigen/src/Geometry/AngleAxis.h"
+#include <Eigen/src/Core/Matrix.h>
+#include <ostream>
+#include <ros/console.h>
 
 #include <cmath>
 #include <sensor_msgs/PointCloud2.h>
 
+#include <sstream>
 #include <string>
 
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/time.h>
-// #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
 
+void rosPrintMatrix4dInfo(Eigen::Matrix4d &matrix){
+    std::ostringstream oss;
+    oss << "Transformation matrix: " << std::endl << "| ";
+    for (int i = 0; i<4; i++){
+        for (int j = 0; j<4; j++){
+            oss << "(" <<matrix(i,j) << ")   ";
+        }
+        oss << "  |" << std::endl;
+        if (i < 3){
+            oss << "|  ";
+        }
+    }
+    ROS_INFO("%s", oss.str().c_str());
+}
+
+Eigen::Matrix4d getFullTransformationMatrix(double roll, double pitch, double yaw,
+                                            double xTrans, double yTrans, double zTrans){
+
+    // Rotation
+    Eigen::AngleAxisd zRotation = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+    Eigen::AngleAxisd yRotation = Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd xRotation = Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX());
+    Eigen::Matrix3d zyxRotationMatrix = zRotation.toRotationMatrix() * yRotation.toRotationMatrix() * xRotation.toRotationMatrix();
+
+    // Translation
+    Eigen::Vector3d translationVec{xTrans, yTrans, zTrans};
+
+    Eigen::Matrix4d zyxtTransformationMatrix = Eigen::Matrix4d::Identity();
+    zyxtTransformationMatrix.block<3, 3>(0, 0) = zyxRotationMatrix;
+    zyxtTransformationMatrix.block<3, 1>(0, 3) = translationVec;
+    return zyxtTransformationMatrix;
+}
 
 ICP::ICP(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private){
 
@@ -31,10 +67,18 @@ void ICP::callback(const sensor_msgs::PointCloud2ConstPtr &msgCloud){
 
     PCLPointCloud::Ptr pclCloudRaw(new PCLPointCloud);
     PCLPointCloud::Ptr pclCloudTransformed(new PCLPointCloud);
-
     pcl::fromROSMsg(*msgCloud, *pclCloudRaw);
-    bool ICM_status = this->applyICM(pclCloudRaw, pclCloudTransformed); // Test 
 
+    // Generate transformation matrix
+    Eigen::Matrix4d transformationMatrix = getFullTransformationMatrix(0, 0, M_PI/4,
+                                                                       0, 0.5, 0);
+    
+    // Apply ICM transformation
+    ROS_INFO("Applying ICM on pointcloud.");
+    rosPrintMatrix4dInfo(transformationMatrix);
+    bool ICM_status = this->applyICM(pclCloudRaw, pclCloudTransformed, transformationMatrix);
+
+    // Convert transfomred point cloud to rosmsg
     sensor_msgs::PointCloud2 msgTransformedCloud;
     pcl::toROSMsg(*pclCloudTransformed, msgTransformedCloud);
 
@@ -42,25 +86,15 @@ void ICP::callback(const sensor_msgs::PointCloud2ConstPtr &msgCloud){
     this->pub_cloud_original_.publish(msgCloud); // Publish the original pointcloud at the same time to compare (because of delay)
 }
 
-// Execute ICP algorithm
-bool ICP::applyICM(PCLPointCloud::Ptr &pclCloudIn, PCLPointCloud::Ptr &pclCloudOut){
+bool ICP::applyICM(PCLPointCloud::Ptr &pclCloudIn,
+                   PCLPointCloud::Ptr &pclCloudOut,
+                   Eigen::Matrix4d &transformationMatrix){
 
     pcl:pcl::console::TicToc time;
 
-    // Transformation matrix
-    Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
-    // Rotation
-    double theta = M_PI / 4; // angle of rotation
-    transformationMatrix(0,0) = std::cos(theta);
-    transformationMatrix(0,1) = -std::sin(theta);
-    transformationMatrix(1,0) = std::sin(theta);
-    transformationMatrix(1,1) = std::cos(theta);
-    // Translation
-    // transformationMatrix(0,3) = 0.4; // x
-    transformationMatrix(1,3) = 0.4; // y
-    // transformationMatrix(2,3) = 0.4; // z
-
-    pcl::transformPointCloud(*pclCloudIn, *pclCloudOut, transformationMatrix);
+    pcl::transformPointCloud(*pclCloudIn,
+                             *pclCloudOut,
+                             transformationMatrix);
 
     int iterations = 1;
 
@@ -73,11 +107,10 @@ bool ICP::applyICM(PCLPointCloud::Ptr &pclCloudIn, PCLPointCloud::Ptr &pclCloudO
 
     icp.align(*pclCloudOut);
 
-    ROS_INFO("Applied %d iterations, elapsed time: %.2f", iterations ,time.toc());
+    ROS_INFO("Applied %d iterations, elapsed time: %.2f", iterations , time.toc());
 
     if (icp.hasConverged()){
         ROS_INFO("ICP has converged, score is %.2f", icp.getFitnessScore());
-        // transformationMatrix = icp.getFinalTransformation().cast<double>();
         return 0;
     }
     else {
@@ -85,3 +118,4 @@ bool ICP::applyICM(PCLPointCloud::Ptr &pclCloudIn, PCLPointCloud::Ptr &pclCloudO
         return 1;
     }
 }
+
