25c28bc6ca8209465978572bf9f4cdd98981382f
Vegar <Vegar1201@live.no>
2024-11-27 15:17:07 +0000

    Add transformation gen funciton and print matrix function.

--- assignments_ws2/src/iterative_closest_point/src/icp.cpp
+++ assignments_ws2/src/iterative_closest_point/src/icp.cpp
@@ -7,42 +7,44 @@
 // #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/voxel_grid.h>
 
-// void rosPrintMatrix4dInfo(Eigen::Matrix4d &matrix){
-//     std::ostringstream oss;
-//     oss << "Transformation matrix: " << std::endl << "| ";
-//     for (int i = 0; i<4; i++){
-//         for (int j = 0; j<4; j++){
-//             oss << "(" <<matrix(i,j) << ")   ";
-//         }
-//         oss << "  |" << std::endl;
-//         if (i < 3){
-//             oss << "|  ";
-//         }
-//     }
-//     // ROS_INFO("%s", oss.str().c_str());
-// }
-//
-// Eigen::Matrix4d getFullTransformationMatrix(double roll, double pitch, double
-// yaw,
-//                                         double xTrans, double yTrans, double
-//                                         zTrans){
-//
-//     // Rotation
-//     Eigen::AngleAxisd zRotation = Eigen::AngleAxisd(yaw,
-//     Eigen::Vector3d::UnitZ()); Eigen::AngleAxisd yRotation =
-//     Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()); Eigen::AngleAxisd
-//     xRotation = Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX());
-//     Eigen::Matrix3d zyxRotationMatrix = zRotation.toRotationMatrix() *
-//     yRotation.toRotationMatrix() * xRotation.toRotationMatrix();
-//
-//     // Translation
-//     Eigen::Vector3d translationVec{xTrans, yTrans, zTrans};
-//
-//     Eigen::Matrix4d zyxtTransformationMatrix = Eigen::Matrix4d::Identity();
-//     zyxtTransformationMatrix.block<3, 3>(0, 0) = zyxRotationMatrix;
-//     zyxtTransformationMatrix.block<3, 1>(0, 3) = translationVec;
-//     return zyxtTransformationMatrix;
-// }
+void rosPrintMatrix4dInfo(Eigen::Matrix4d &matrix){
+    std::ostringstream oss;
+    oss << "Transformation matrix: " << std::endl << "| ";
+    for (int i = 0; i<4; i++){
+        for (int j = 0; j<4; j++){
+            oss << "(" <<matrix(i,j) << ")   ";
+        }
+        oss << "  |" << std::endl;
+        if (i < 3){
+            oss << "|  ";
+        }
+    }
+    // ROS_INFO("%s", oss.str().c_str());
+}
+
+
+Eigen::Matrix4d getFullTransformationMatrix(double roll, double pitch,
+                                            double yaw, double xTrans,
+                                            double yTrans, double zTrans) {
+    // Rotation
+    Eigen::AngleAxisd zRotation =
+        Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+    Eigen::AngleAxisd yRotation =
+        Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd xRotation =
+        Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX());
+    Eigen::Matrix3d zyxRotationMatrix = zRotation.toRotationMatrix() *
+                                        yRotation.toRotationMatrix() *
+                                        xRotation.toRotationMatrix();
+
+    // Translation
+    Eigen::Vector3d translationVec{xTrans, yTrans, zTrans};
+
+    Eigen::Matrix4d zyxtTransformationMatrix = Eigen::Matrix4d::Identity();
+    zyxtTransformationMatrix.block<3, 3>(0, 0) = zyxRotationMatrix;
+    zyxtTransformationMatrix.block<3, 1>(0, 3) = translationVec;
+    return zyxtTransformationMatrix;
+}
 
 PCLPointCloud::Ptr downSample(PCLPointCloud::Ptr &raw_cloud) {
 
