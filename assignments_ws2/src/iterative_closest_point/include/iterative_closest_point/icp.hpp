91466305b8e12cb6bde6b7f338f175399c8f0f25
Vegar <Vegar1201@live.no>
2024-11-27 14:23:37 +0000

    Define icp node class and the logic functions from assignment1.

--- assignments_ws2/src/iterative_closest_point/include/iterative_closest_point/icp.hpp
+++ assignments_ws2/src/iterative_closest_point/include/iterative_closest_point/icp.hpp
@@ -1,9 +1,38 @@
 #ifndef ICP_H
 #define ICP_H
 
-#include <rclcpp/rclcpp.h>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include <rclcpp/subscription.hpp>
+// #include <sensor_msgs/msg/point_cloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+typedef pcl::PointXYZ PCLPoint;
+typedef pcl::PointCloud<PCLPoint> PCLPointCloud;
+
+class ICP_node : public rclcpp::Node {
+  public:
+    ICP_node();
+
+    void raw_cloud_callback(
+        const sensor_msgs::msg::PointCloud2::SharedPtr raw_cloud);
+
+  private:
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr
+        sub_raw_cloud_;
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr
+        pub_cloud_original;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr
+        pub_cloud_transfomred_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr
+        pub_algigned_cloud_;
+};
+
+PCLPointCloud::Ptr downSample(PCLPointCloud::Ptr &raw_cloud);
+bool applyICM(PCLPointCloud::Ptr &pclCloudSource,
+              PCLPointCloud::Ptr &pclCloudTarget);
 
 #endif // !ICP_H
 #define ICP_H
-
-
